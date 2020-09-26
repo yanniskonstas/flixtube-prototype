c9d9235a8e9084df358a2fd4a1cc148189a68e77
yanniskonstas <konstas@protonmail.com>
2020-09-26 13:23:47 +0100

    first commit

---
+++ scripts/variables.tf
@@ -0,0 +1,23 @@
+# Sets global variables for this Terraform project.
+
+variable app_name {
+    default = "flixtubeuk"
+}
+variable location {
+  default = "UK South"
+}
+
+variable admin_username {
+  default = "linux_admin"
+}
+
+variable app_version { # Can't be called version! That's a reserved word.
+}
+
+variable client_id {
+
+}
+
+variable client_secret {
+
+}
